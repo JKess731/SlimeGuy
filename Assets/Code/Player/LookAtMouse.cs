5bf81758e31eadf03d836fae9dda4f625998c20b
Edison Li <72610128+Excailibur@users.noreply.github.com>
2024-02-07 18:18:59 -0500

    Ring pointer

--- Assets/Code/Player/LookAtMouse.cs
+++ Assets/Code/Player/LookAtMouse.cs
@@ -8,8 +8,15 @@ public class LookAtMouse : MonoBehaviour
 {
     void Update()
     {
-        Vector3 lookDir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-        float lookAngle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(lookAngle, Vector3.forward);
+        //Vector3 lookDir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        //float lookAngle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.AngleAxis(lookAngle, Vector3.forward);
+
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.WorldToScreenPoint(mousePosition);
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x,
+                                        mousePosition.y - transform.position.y);
+
+        transform.up = direction;
     }
 }
