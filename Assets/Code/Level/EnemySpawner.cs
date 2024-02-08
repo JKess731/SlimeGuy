a5b864cd02252158262e0dc253a8aef74e8c9295
Kessler, Jared <jkessler731@gmail.com>
2024-02-08 14:08:27 -0500

    base enemy spawner

---
+++ Assets/Code/Level/EnemySpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] private List<GameObject> spawners = new List<GameObject>();
+    [SerializeField] private List<GameObject> enemies = new List<GameObject>();
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        foreach (GameObject spawner in spawners)
+        {
+            int indx = Random.Range(0, enemies.Count - 1);
+
+            // Choose an enemy
+            GameObject chosenEnemy = enemies[indx];
+
+            // Find a spot in the spawn area
+            //Vector2 enemySpawnLoc = chooseSpawnLoc(spawner.GetComponent<CircleCollider2D>());
+
+            //Spawn enemy
+            GameObject enemy = Instantiate(chosenEnemy);
+            enemy.transform.position = spawner.transform.position;
+            enemy.layer = 6;
+            //enemy.transform.position = enemySpawnLoc;
+
+        }
+
+    }
+
+    //private Vector2 chooseSpawnLoc(CircleCollider2D radius)
+    //{
+    //    Vector2 spawnLoc = Random.insideUnitCircle * radius.radius;
+
+    //    return spawnLoc;
+
+    //}
+
+}
