46068ddb9e298ecde5df86e9dbc8512167db6bcf
Edison Li <72610128+Excailibur@users.noreply.github.com>
2024-02-19 17:28:27 -0500

    Summary xxl

--- Assets/Code/Player/player.cs
+++ Assets/Code/Player/player.cs
@@ -6,7 +6,11 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     private stats playerStats = new stats(10, 10, 10, 10);
-
+    /// <summary>
+    /// Gets the value of player's stat based on stats.statsName
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns> Value of the stat </returns>
     public int returnStats(stats.statsName type)
     {
         switch (type)
@@ -22,7 +26,11 @@ public class player : MonoBehaviour
         }
         return 0;
     }
-
+    /// <summary>
+    /// Increase the given stat based on stats.statBoost
+    /// </summary>
+    /// <param name="amount"> The growth rate of the given stat</param>
+    /// <param name="type"> The type of Stat Boost</param>
     public void increaseStats(int amount, stats.statBoost type)
     {
         switch (type)
