e2742a70a517e8ec244aed7088486af19a2fb32b
Edison Li <72610128+Excailibur@users.noreply.github.com>
2024-02-20 10:37:11 -0500

    Merge

--- Assets/Code/Level/RoomTriggerSpawner.cs
+++ Assets/Code/Level/RoomTriggerSpawner.cs
@@ -13,34 +13,9 @@ public class RoomTriggerSpawner : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-<<<<<<< HEAD
-        
-        SlimeSplit splitAbility = player.GetComponent<SlimeSplit>();
-
-        foreach (GameObject spawner in spawners)
-        {
-            int indx = Random.Range(0, enemies.Count - 1);
-
-            // Choose an enemy
-            GameObject chosenEnemy = enemies[indx];
-
-            // Find a spot in the spawn area
-            //Vector2 enemySpawnLoc = chooseSpawnLoc(spawner.GetComponent<CircleCollider2D>());
-
-            //Spawn enemy
-            GameObject enemy = Instantiate(chosenEnemy);
-            enemy.transform.position = spawner.transform.position;
-            enemy.layer = 7;
-            
-            splitAbility.enemiesInRoom.Add(enemy);
-
-        }
-=======
->>>>>>> 58b017070eb7097b7c9b4132a9e047be9107fa2f
 
         roomTriggerControl.SpawnEnemies(roomTriggerControl.spawners);
         Destroy(gameObject);
 
     }
-
 }
