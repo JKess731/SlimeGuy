273db5cb95ff52dce24bf4bfa775e368619d9b85
Excailibur <liedison360@gmail.com>
2024-02-28 13:02:04 -0500

    EnemyAttackRam

--- Assets/Code/Enemy/enemyAttackS/EnemyAttackSlam1.cs
+++ Assets/Code/Enemy/enemyAttackS/EnemyAttackSlam1.cs
@@ -48,7 +48,6 @@ public class EnemyAttackSlam : MonoBehaviour
             enemy.GetComponentInParent<EnemyMovementFollow>().AttackNotColliding();
 
             attackCon = false;
-
         }
     }
 
