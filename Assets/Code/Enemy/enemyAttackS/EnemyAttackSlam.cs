3d563328ead68730f83d373706f09a2bb5f0a69d
Excailibur <liedison360@gmail.com>
2024-02-28 13:08:32 -0500

    CS and meta fix part 2

--- Assets/Code/Enemy/enemyAttackS/EnemyAttackSlam.cs
+++ Assets/Code/Enemy/enemyAttackS/EnemyAttackSlam.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class EnemyAttackSlam1 : MonoBehaviour
+public class EnemyAttackSlam : MonoBehaviour
 {
     [SerializeField] private int damage;
     [SerializeField] private float attackDelay;
@@ -20,14 +20,12 @@ public class EnemyAttackSlam1 : MonoBehaviour
         playerMove = GameObject.FindWithTag("player").GetComponent<PlayerMove>();
     }
 
-
     void Update()
     {
         Vector3 rotation = player.transform.position - ring.transform.position;
         float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
         ring.transform.rotation = Quaternion.Euler(0, 0, rotZ);
     }
-
 
     //Handles attack collision
 
@@ -50,7 +48,6 @@ public class EnemyAttackSlam1 : MonoBehaviour
             enemy.GetComponentInParent<EnemyMovementFollow>().AttackNotColliding();
 
             attackCon = false;
-
         }
     }
 
@@ -61,7 +58,6 @@ public class EnemyAttackSlam1 : MonoBehaviour
             yield return new WaitForSeconds(attackDelay/2);
             ring.GameObject().GetComponent<SpriteRenderer>().enabled = true;            player.GetComponentInParent<PlayerHealth>().Damage(damage);
             StartCoroutine(PlayerStunned());
-            
         }
         
     }
