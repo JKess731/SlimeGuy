99050eb22560f3107e639c38626f0158c7707dda
Excailibur <liedison360@gmail.com>
2024-02-28 11:50:31 -0500

    Revert "Removed the source of our missery"
    This reverts commit eef3ef6722a23d6f3f8f3704216956102a176b04.

---
+++ Assets/Code/Enemy/enemyAttackS/enemyBullet.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class EnemyBullet : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private GameObject player;
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private int bulletDamage;
+    public Vector2 playerpos = Vector2.zero;
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("player");
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("player");
+        Vector3 direction = player.transform.position - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * bulletSpeed;
+
+        float rot = Mathf.Atan2(-direction.x, -direction.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot);
+        Destroy(gameObject, 3f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerHealth>().Damage(bulletDamage);
+            Debug.Log("Bullet dmg");
+            Destroy(gameObject);
+        }
+    }
+}
