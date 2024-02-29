59ee14edec6c2c47de6b3e6abd39760293aa10e9
Edison Li <72610128+Excailibur@users.noreply.github.com>
2024-02-28 21:27:39 -0500

    slimesplit scrip 1

---
+++ Assets/Code/Player/Split/SlimeSplit1.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Jared Kessler
+public class SlimeSplit1 : MonoBehaviour
+{
+    [SerializeField] private int minionCounter = 1;
+    [SerializeField] private GameObject minion;
+
+    public List<GameObject> enemiesInRoom = new List<GameObject>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Temp Input
+        if (Input.GetKeyDown(KeyCode.R)) 
+        {
+            if (minionCounter > 0)
+            {
+                GameObject newMinion = OnSplit();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Generates a minion object
+    /// </summary>
+    /// <returns></returns>
+    private GameObject OnSplit()
+    {
+        minionCounter--;
+        // Spawn Minion at player location
+        GameObject spawnedMinion = Instantiate(minion);
+        spawnedMinion.transform.position = transform.position;
+
+        return spawnedMinion;
+    }
+}
