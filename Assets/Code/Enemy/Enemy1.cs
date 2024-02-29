8ce6d7e991076d090cd8a26e412bbcf8c085216c
Edison Li <72610128+Excailibur@users.noreply.github.com>
2024-02-28 20:06:31 -0500

    push

---
+++ Assets/Code/Enemy/Enemy1.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Edison Li
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private int maxHealth;
+    [SerializeField] private int health;
+
+    [SerializeField] private int attackDamage;
+    [SerializeField] private float speed;
+    [SerializeField] private int defense;
+    [SerializeField] public int dangerLevel;
+
+    //Set health to max
+    private void Awake()
+    {
+        health = maxHealth;
+    }
+
+    //Handles Damage
+    public void Damage(int damage)
+    {
+        Debug.Log(damage);
+        Debug.Log("Taking damage");
+        Debug.Log(gameObject.name + ":" + health);
+        damage = damage - defense;
+        if (damage <= 0) {  damage = 1; }
+        health = health - damage;
+        Debug.Log("Enemy taking: " + damage);
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+}
