5ce7b140bf901278f4d1acf4bb732972eaa3b103
Edison Li <72610128+Excailibur@users.noreply.github.com>
2024-02-28 21:18:27 -0500

    New attempt

--- Assets/Code/Player/BrandNew/PlayerScript1.cs
+++ Assets/Code/Player/BrandNew/PlayerScript1.cs
@@ -2,47 +2,47 @@ using UnityEngine;
 
 public class PlayerScript1 : MonoBehaviour
 {
-    private Stats playerStats = new Stats(10, 10, 10, 10);
+    private Stats1 playerStats = new Stats1(10, 10, 10, 10);
 
     /// <summary>
-    /// Gets the value of player's stat based on Stats.statsName
+    /// Gets the value of player's stat based on Stats1.statsName
     /// </summary>
     /// <param name="type"></param>
     /// <returns> Value of the stat </returns>
-    public int returnStats(Stats.statsName type)
+    public int returnStats(Stats1.statsName type)
     {
         switch (type)
         {
-            case Stats.statsName.MAXHEALTH:
+            case Stats1.statsName.MAXHEALTH:
                 return playerStats.maxHealth;
-            case Stats.statsName.ATTACK:
+            case Stats1.statsName.ATTACK:
                 return playerStats.attack;
-            case Stats.statsName.DEFENSE:
+            case Stats1.statsName.DEFENSE:
                 return playerStats.defense;
-            case Stats.statsName.SPEED:
+            case Stats1.statsName.SPEED:
                 return playerStats.speed;
         }
         return 0;
     }
     /// <summary>
-    /// Increase the given stat based on Stats.statBoost
+    /// Increase the given stat based on Stats1.statBoost
     /// </summary>
     /// <param name="amount"> The growth rate of the given stat</param>
     /// <param name="type"> The type of Stat Boost</param>
-    public void increaseStats(int amount, Stats.statBoost type)
+    public void increaseStats(int amount, Stats1.statBoost type)
     {
         switch (type)
         {
-            case Stats.statBoost.healthUP:
+            case Stats1.statBoost.healthUP:
                 playerStats.maxHealth += amount;
                 break;
-            case Stats.statBoost.attackUP:
+            case Stats1.statBoost.attackUP:
                 playerStats.attack += amount;
                 break;
-            case Stats.statBoost.defenseUP:
+            case Stats1.statBoost.defenseUP:
                 playerStats.defense += amount;
                 break;
-            case Stats.statBoost.speedUP:
+            case Stats1.statBoost.speedUP:
                 playerStats.speed += amount;
                 break;
         }
