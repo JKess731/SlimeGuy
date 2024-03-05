cade423dc5c69c8c25b12884524825832b1c14f9
B-Sheley <112135501+B-Sheley@users.noreply.github.com>
2024-03-04 20:07:22 -0500

    Small Dwarf bug fixing attempt + Golem rock volley + Golem behavior script + Golem attack cycle

--- Assets/Code/Enemy/GolemBehavior.cs
+++ Assets/Code/Enemy/GolemBehavior.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class DwarfBehavior : MonoBehaviour
+public class GolemBehavior : MonoBehaviour
 {
     //Enemy script.
     [SerializeField] private float speed;
@@ -18,141 +18,194 @@ public class DwarfBehavior : MonoBehaviour
     private bool foundPlayer = false;
     public float stop;
 
-
-    //EnemyAttackSlash script.
-    [SerializeField] private int slashDamage;
-    [SerializeField] private float attackDelay;
+    //EnemyAttackSlam script
+    [SerializeField] private int slamDamage;
+    [SerializeField] private int volleyDamage;
+    [SerializeField] private float slamAttackDelay;
+    public PlayerMove playerMove;
     private bool attacking = false;
     public GameObject ring;
 
-    //EnemyAttackThrowBoomerang script.
-    public GameObject boomerangObject;
-    public Transform boomerangPos;
+    //RockVolley
+    [SerializeField] private float rockVolleyAttackDelay;
+    public GameObject rockFallLocation;
+    public GameObject frontLocation;
     private float shotCooldown;
     public float startShotCooldown;
     private Vector3 lastPosition = new Vector3();
     public float detectRange;
 
+
     //Attack cycles
     private float attackCounter = 0;
     private bool attackOnce = true;
 
+
     private void Awake()
     {
         player = GameObject.FindWithTag("player").transform;
+        playerMove = GameObject.FindWithTag("player").GetComponent<PlayerMove>();
         shotCooldown = startShotCooldown;
         StartCoroutine(AttackCycleSwitch());
     }
 
-    // Update is called once per frame
-    void Update(){
+
+    void Update()
+    {
 
         //From EnemyMovementFollow script.
         distance = Vector2.Distance(transform.position, player.position);
         Vector2 direction = player.position - transform.position;
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x);
-        if (attackCounter > .5){
-            if (distance < distanceBetween){
-                if (distance > stop){
+        if (attackCounter > .5)
+        {
+            if (distance < distanceBetween)
+            {
+                if (distance > stop)
+                {
                     transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
                     transform.rotation = Quaternion.Euler(Vector3.forward * angle);
                 }
             }
 
-            //From EnemyAttackThrowBoomerang Script
+            //RockVolleyMovementBehavior
             float boomerangDistance = Vector2.Distance(transform.position, player.transform.position);
             Vector3 boomerangRotation = player.transform.position - ring.transform.position;
             float rotZ = Mathf.Atan2(boomerangRotation.y, boomerangRotation.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0, 0, rotZ);
-            if (distance < detectRange){
-                if (transform.position == lastPosition){
-                    if (shotCooldown <= 0){
-                        if(attackOnce == true) {
+            if (distance < detectRange)
+            {
+                if (transform.position == lastPosition)
+                {
+                    if (shotCooldown <= 0)
+                    {
+                        if (attackOnce == true)
+                        {
                             attackOnce = false;
-                            StartCoroutine(ThrowAttack());
-                        }  
+                            StartCoroutine(RockVolleyAttack());
+                        }
                     }
-                    else{
+                    else
+                    {
                         shotCooldown -= Time.deltaTime;
                     }
                 }
-                else{
+                else
+                {
                     shotCooldown = startShotCooldown;
                 }
                 lastPosition = transform.position;
             }
             lastPosition = transform.position;
         }
-        else{
-            if (onPlayer == false){
-                if (foundPlayer == false){
-                    if (distance < distanceBetween){
+        else
+        {
+            if (onPlayer == false)
+            {
+                if (foundPlayer == false)
+                {
+                    if (distance < distanceBetween)
+                    {
                         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
                         transform.rotation = Quaternion.Euler(Vector3.forward * angle);
                         foundPlayer = true;
                     }
                 }
-                else{
+                else
+                {
                     transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
                     transform.rotation = Quaternion.Euler(Vector3.forward * angle);
                 }
             }
 
-            //From EnemyAttackSlash script.
-            if (attacking == false){
+            //From EnemyAttackSlam script.
+            if (attacking == false)
+            {
                 Vector3 rotation = player.transform.position - ring.transform.position;
-                float slashRotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-                ring.transform.rotation = Quaternion.Euler(0, 0, slashRotZ);
+                float RotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+                ring.transform.rotation = Quaternion.Euler(0, 0, RotZ);
             }
-        } 
-    }
 
+        }
+    }
 
     //---------------------------------------------------------------------------------------------------------------------------
     //THIS SECTION HANDLES FUNCTIONS FROM EnemyMovementFollow script.
-    public void AttackColliding(){
+    public void AttackColliding()
+    {
         onPlayer = true;
     }
 
-    public void AttackNotColliding(){
+    public void AttackNotColliding()
+    {
         onPlayer = false;
     }
 
 
     //---------------------------------------------------------------------------------------------------------------------------
-    //THIS SECTION HANDLES SLASH ATTACK COLLISION AND COROUTINE from EnemyAttackSlash script.
-    private void OnTriggerEnter2D(Collider2D collision){
-        if (collision.gameObject.name == "Player"){
-            AttackColliding();
-            StartCoroutine(SlashAttackingContinue());
+    //THIS SECTION HANDLES FUNCTIONS FROM EnemyAttackSlam script.
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (attackCounter > .5)
+        {
+            if (collision.gameObject.name == "Player")
+            {
+                Debug.Log("Player has been hit by volley");
+                player.GetComponentInParent<PlayerHealth>().Damage(volleyDamage);
+            }
+        }
+        else 
+        {
+            if (collision.gameObject.name == "Player")
+            {
+                AttackColliding();
+                StartCoroutine(SlamAttackingContinue());
+            }
         }
     }
 
-
-    IEnumerator SlashAttackingContinue(){
+    IEnumerator SlamAttackingContinue()
+    {
         attacking = true;
-        yield return new WaitForSeconds(attackDelay / 2);
+        yield return new WaitForSeconds(slamAttackDelay / 2);
         ring.GameObject().GetComponent<SpriteRenderer>().enabled = false;
-        yield return new WaitForSeconds(attackDelay / 2);
+        yield return new WaitForSeconds(slamAttackDelay / 2);
         ring.GameObject().GetComponent<SpriteRenderer>().enabled = true;
-        player.GetComponentInParent<PlayerHealth>().Damage(slashDamage);
+        player.GetComponentInParent<PlayerHealth>().Damage(slamDamage);
+        StartCoroutine(PlayerStunned());
         attacking = false;
         RestartAttackCounter();
         AttackNotColliding();
 
 
 
+    }
+
+    IEnumerator PlayerStunned()
+    {
+        playerMove.input.Disable();
+        Debug.Log("Player Stunned");
+        yield return new WaitForSeconds(slamAttackDelay / 2);
+        playerMove.input.Enable();
+    }
+
+
+
+    //---------------------------------------------------------------------------------------------------------------------------
+    //THIS HANDLES COROUTINE FOR ROCKVOLLEY.
+    IEnumerator RockVolleyAttack()
+    {
+        yield return new WaitForSeconds(rockVolleyAttackDelay);
+        rockFallLocation.transform.position = player.transform.position;
+        yield return new WaitForSeconds(rockVolleyAttackDelay/2);
+        rockFallLocation.transform.position = frontLocation.transform.position;
+        RestartAttackCounter();
+        AttackNotColliding();
 
     }
-    //---------------------------------------------------------------------------------------------------------------------------
-    //THIS HANDLES COROUTINE FOR THROWING THE BOOMERANG. FROM EnemyAttackThrowBommerang SCRIPT.
-    IEnumerator ThrowAttack(){
-        yield return new WaitForSeconds(1f);
-        GameObject bullet1 = Instantiate(boomerangObject, boomerangPos.position, ring.transform.rotation);
-        bullet1.gameObject.GetComponent<EnemyBulletReturn>().enemy = enemy;
-        yield return new WaitForSeconds(1f);
-    }
+
 
     //---------------------------------------------------------------------------------------------------------------------------
     //THIS HANDLES ATTACK CYCLES
@@ -165,12 +218,12 @@ public class DwarfBehavior : MonoBehaviour
         //> .5 = Throw
     }
 
-    public void RestartAttackCounter() {
+    public void RestartAttackCounter()
+    {
         onPlayer = false;
         attackCounter = 0;
         StartCoroutine(AttackCycleSwitch());
         attackOnce = true;
         shotCooldown = startShotCooldown;
     }
-
 }
