a69d33a26b6290a881eb4869e541832a004835cb
Graham, Noah T <noahgrahm@gmail.com>
2024-08-28 17:36:05 -0400

    Basic Ability UI done
    Made a UI for ability slots, ability icons populate those slots, made a cooldown radial timer once the abilities are used.

--- Assets/Code/UI/UIManager.cs
+++ Assets/Code/UI/UIManager.cs
@@ -11,6 +11,20 @@ public class UiManager : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI healthTxt;
     [SerializeField] private TextMeshProUGUI absorptionTxt;
+    [SerializeField] private TextMeshProUGUI primaryCooldownTxt;
+    [SerializeField] private TextMeshProUGUI secondaryCooldownTxt;
+    [SerializeField] private TextMeshProUGUI dashCooldownTxt;
+
+    [SerializeField] private Image primaryAbilityImage;
+    [SerializeField] private Image secondaryAbilityImage;
+    [SerializeField] private Image dashAbilityImage;
+    [SerializeField] private Image passiveAbilityImage;
+
+    [SerializeField] private Slider primaryCooldown;
+    [SerializeField] private Slider secondaryCooldown;
+    [SerializeField] private Slider dashCooldown;
+
+    private AbilityManager _abilityManager;
 
     public static UiManager instance;
 
@@ -25,11 +39,14 @@ public class UiManager : MonoBehaviour
             Debug.LogWarning("UIManager instance already exists. Destroying duplicate.");
             Destroy(gameObject);
         }
-
+        _abilityManager = FindObjectOfType<AbilityManager>();
         healthBar = GameObject.Find("Health Bar").GetComponent<Slider>();
         absorptionBar = GameObject.Find("Absorption Bar").GetComponent<Slider>();
         healthTxt = GameObject.Find("Health Text").GetComponent<TextMeshProUGUI>();
         absorptionTxt = GameObject.Find("Absorption Text").GetComponent<TextMeshProUGUI>();
+        UpdatePrimaryAbilityImage(_abilityManager.Primary.Icon);
+        UpdateSecondaryAbilityImage(_abilityManager.Secondary.Icon);
+        UpdateDashAbilityImage(_abilityManager.Dash.Icon);
     }
 
     public void UpdateHealthBar(float health, float maxHealth)
@@ -42,5 +59,69 @@ public class UiManager : MonoBehaviour
     {
         absorptionBar.value = absorption / maxAbsorption;
         absorptionTxt.SetText(absorption + "/" + maxAbsorption);
+    }
+
+    public void UpdatePrimaryAbilityImage(Sprite Icon)
+    {
+        primaryAbilityImage.sprite = Icon;
+    }
+
+    public void UpdateSecondaryAbilityImage(Sprite Icon)
+    {
+        secondaryAbilityImage.sprite = Icon;
+    }
+
+    public void UpdateDashAbilityImage(Sprite Icon)
+    {
+        dashAbilityImage.sprite = Icon;
+    }
+
+    public void UpdatePassiveAbilityImage(Sprite Icon)
+    {
+        passiveAbilityImage.sprite = Icon;
+    }
+
+    public IEnumerator TextAndSliderAdjustment(AbilityBase attack, string type)
+    {
+        Debug.Log("I got to TextAndSliderAdjustment");
+        Slider modifiedSlider = null;
+        TextMeshProUGUI modifiedText = null;
+        float newValue = attack.Behavior.CooldownTime;
+        if(type == "P")
+        {
+            modifiedSlider = primaryCooldown;
+            modifiedText = primaryCooldownTxt;
+        }
+        else if(type == "S")
+        {
+            modifiedSlider = secondaryCooldown;
+            modifiedText = secondaryCooldownTxt;
+        }
+        else if(type == "D")
+        {
+            modifiedSlider = dashCooldown;
+            modifiedText = dashCooldownTxt;
+        }
+        yield return new WaitForSeconds(attack.Behavior.ActivationTime);
+
+        modifiedSlider.gameObject.SetActive(true);
+        modifiedText.gameObject.SetActive(true);
+
+        modifiedSlider.maxValue = attack.Behavior.CooldownTime;
+        modifiedSlider.value = modifiedSlider.maxValue;
+        modifiedText.text = attack.Behavior.CooldownTime.ToString(); 
+
+        while(newValue > 0)
+        {
+            newValue -= 1;
+            yield return new WaitForSeconds(1);
+            modifiedSlider.value = newValue;
+            modifiedText.text = newValue.ToString();
+        }
+
+        modifiedSlider.gameObject.SetActive(false);
+        modifiedText.gameObject.SetActive(false);
+        modifiedSlider.maxValue = 1;
+        modifiedText.text = "";
     }
 }
