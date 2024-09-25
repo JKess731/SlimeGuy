7403910dee1d9fee51d99085463e0229c8e0ec1f
Kessler, Jared <jkessler731@gmail.com>
2024-09-24 22:36:17 -0400

    enemy updates

--- Assets/Code/Enemy/BehaviorLogic/Attack/Scripts/EnemyAttackFrontSlash.cs
+++ Assets/Code/Enemy/BehaviorLogic/Attack/Scripts/EnemyAttackFrontSlash.cs
@@ -17,6 +17,34 @@ public class EnemyAttackFrontSlash : EnemyAttackSOBase
     {
         base.DoAnimationTriggerEventLogic(triggerType);
 
+        GameObject slash = Instantiate(slashTriggerPrefab, attackPoint.position, ring.transform.rotation);
+        _enemy.stateMachine.ChangeState(_enemy.chaseState);
+
+        Vector3 rotation = _playerTransform.position - ring.transform.position;
+        float slashRotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        ring.transform.rotation = Quaternion.Euler(0, 0, slashRotZ);
+
+        if (triggerType == EnemyBase.AnimationTriggerType.DwarfAttack)
+        {
+            AudioManager.instance.PlayOneShot(FmodEvents.instance.DwarfAttack, _transform.position);
+        }
+
+        if (triggerType == EnemyBase.AnimationTriggerType.DwarfDamaged)
+        {
+            AudioManager.instance.PlayOneShot(FmodEvents.instance.DwarfHurt, _transform.position);
+        }
+
+        if (triggerType == EnemyBase.AnimationTriggerType.DwarfDeath)
+        {
+            AudioManager.instance.PlayOneShot(FmodEvents.instance.DwarfDeath, _transform.position);
+        }
+
+        if (triggerType == EnemyBase.AnimationTriggerType.GolemAttack)
+        {
+            AudioManager.instance.PlayOneShot(FmodEvents.instance.GolemAttack, _transform.position);
+            rotation = new Vector3(0, 0, 0);
+        }
+
     }
 
     public override void DoEnterLogic()
@@ -36,19 +64,6 @@ public class EnemyAttackFrontSlash : EnemyAttackSOBase
     {
         base.DoFrameUpdateLogic();
         _enemy.MoveEnemy(Vector2.zero);
-        if (timer > frontSlashAttackDelay)
-        {
-            timer = 0f;
-            GameObject slash = Instantiate(slashTriggerPrefab, attackPoint.position, ring.transform.rotation);
-            _enemy.stateMachine.ChangeState(_enemy.chaseState);
-        }
-        if (timer < frontSlashAttackDelay)
-        {
-            Vector3 rotation = _playerTransform.position - ring.transform.position;
-            float slashRotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-            ring.transform.rotation = Quaternion.Euler(0, 0, slashRotZ);
-        }
-        timer += Time.deltaTime;
     }
 
     public override void DoPhysicsLogic()
@@ -60,6 +75,8 @@ public class EnemyAttackFrontSlash : EnemyAttackSOBase
     {
         base.ResetValues();
     }
+
+
 
 
 }
